1bdbbe7e4782ab80171d077c880725f03dc6ba8d
StevenYang23 <stevenydjm@gmail.com>
2026-04-12 20:00:02 +0800

    2

--- Agent_ZScore_Class.py
+++ Agent_ZScore_Class.py
@@ -1,7 +1,22 @@
 class Agent_ZScore:
-    def __init__(self):
-        self.portfolio_value = 0
-        self.previous_portfolio_value = 0
+    def __init__(self, k = 0.65):
+        self.port_value = 0
+        self.prev_port_value = 0
+        self.num_options = 0 # number of options to trade (1 or 0)
+        self.num_underlying = 0 # number of underlying shares to trade
+        self.k = k # z-score threshold
+        self.vrp_list = []
+        self.delta_list = []
+        self.gamma_list = []
+        self.vega_list = []
+        self.theta_list = []
+        self.vanna_list = []
+        self.volga_list = []
+        self.rho_list = []
 
     def trade(self, data):
-        pass
+        self.prev_port_value = self.port_value
+        self.vrp_list.append(data["Straddle_imp_vol"] - data["RV"])
+        if data['Force_Close']:
+            
+        
